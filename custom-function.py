02c7f801b94eb992fad97e80c3244144ef8090ec
awaismemon313@gmail.com <awaismemon313@gmail.com>
2017-10-30 13:55:02 +0500

    Work done on 29-October-2017

---
+++ custom-function.py
@@ -0,0 +1,10 @@
+def myfunc(b):
+    for Lassi in range(1,b):
+        if Lassi > 5:
+            print("Lassi " + str(Lassi))
+            print("You Win")
+            break
+        else:
+            print("You Lose")
+            break
+myfunc(5)
---
+++ upper-case.py
@@ -0,0 +1,2 @@
+a=("awais")
+print(a.title())
