a36e9970b2d54d086f412b93729acf1a23d5c356
awaismemon313@gmail.com <awaismemon313@gmail.com>
2017-10-31 21:12:15 +0500

    First Program.

---
+++ findprime.py
@@ -0,0 +1,12 @@
+def findprime(num):
+
+    if num > 1:
+
+        for i in range(2, num):
+            if (num % i) == 0:
+                print(num, "is not a prime number")
+
+            else:
+                print(num, "is a prime number")
+
+    findprime(10)
